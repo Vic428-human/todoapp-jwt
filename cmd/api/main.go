542e91f579f7164c7781d6c33fba01f1552f6cda
Vic428-human <z0983195379a@gmail.com>
2026-02-08 22:20:40 +0800

    dont-trust-all-proxies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,8 @@ func main() {
 	// pointer refers to the address or instance in memory, and not copy entire thing
 	var router *gin.Engine = gin.Default() // gin => do client request and response
 	router.GET("/", func(c *gin.Context) {
+		router.SetTrustedProxies(nil) // if you don't use any proxy, you can disable this feature by using nil, then Context.ClientIP() will return the remote address directly to avoid some unnecessary computation
+
 		// gin.H is a shortcut for map[string]interface{} or map[string]any
 		c.JSON(200, gin.H{
 			"message": "todo api running successfully!!!",
