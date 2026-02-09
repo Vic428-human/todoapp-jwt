d6aaa1e24ef550305c4d4adea016a719dfbc53ee
Vic428-human <z0983195379a@gmail.com>
2026-02-09 14:02:47 +0800

    load env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"log"
+	"os" //retrieve environment variables
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseURL string
+	Port        string
+}
+
+// pointer : point to the address or instance in memory
+func Load() (*Config, error) {
+	// loads .env from the current directory
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	/* *pointer (point to a struct) = &reference{} => https://stackoverflow.com/questions/47296325/passing-by-reference-and-value-in-go-to-functions
+		func someFunc(x *int) {
+	    *x = 2 // Whatever variable caller passed in will now be 2
+	    y := 7
+	    x = &y // has no impact on the caller because we overwrote the pointer value!
+	}
+	*/
+	var config *Config = &Config{
+		DatabaseURL: os.Getenv("POSTGRES_URL"),
+		Port:        os.Getenv("POSTGRES_PORT"),
+	}
+	return config, nil
+}
